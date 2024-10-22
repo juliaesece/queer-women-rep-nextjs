2df1b6b974ba02bc216215b35b1561988e1b8ea5
Julia Sepulveda <ecojuliase@gmail.com>
2024-10-22 14:18:45 -0300

    Apostrophe

--- app/_layout-components/Footer.tsx
+++ app/_layout-components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className={st.footer}>
       <p>Hi! This is a work in progress, and is currently not production ready, so you might see some bugs, and the site might break down at times.</p>
-      <p>If you'd like to help by reporting some bugs or suggesting some features, or just vibe, you can join the discord <a href="https://discord.gg/v8uUPXug">here</a>.</p>
+      <p>If you&apos;d like to help by reporting some bugs or suggesting some features, or just vibe, you can join the discord <a href="https://discord.gg/v8uUPXug">here</a>.</p>
     </footer>
   );
 }
