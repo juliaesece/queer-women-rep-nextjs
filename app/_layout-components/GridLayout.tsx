c3463206156d75665c7a607f9698cc9bb3d8487d
Julia Sepulveda <ecojuliase@gmail.com>
2025-01-15 23:29:11 +0000

    Corrected grid layout on desktop

--- app/_layout-components/GridLayout.tsx
+++ app/_layout-components/GridLayout.tsx
@@ -1,29 +1,12 @@
 import Card from "./Card";
+import styles from "./GridLayout.module.css";
 
 export default async function GridLayout({ couples }) {
-
     return (
-        <>
-            <div>
-                {
-                    couples && couples.slice(0, 3).map((couple) =>
-                        <Card couple={couple} key={couple.origin} />
-                    )
-                }
-            </div>
-            <div>
-                {
-                    couples && couples.slice(3, 6).map((couple) =>
-                        <Card couple={couple} key={couple.origin} />
-                    )
-                }
-            </div>
-            <div>
-                {
-                    couples && couples.slice(6, 9).map((couple) =>
-                        <Card couple={couple} key={couple.origin} />
-                    )
-                }
-            </div>
-        </>);
+        <div className={styles.grid}>
+            {couples && couples.map((couple) =>
+                <Card couple={couple} key={couple.origin} />
+            )}
+        </div>
+    );
 }
--- app/info/[id]/page.tsx
+++ app/info/[id]/page.tsx
@@ -1,6 +1,17 @@
 import Modal from "@/app/@modal/(.)info/[id]/Modal"
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../utils/authOptions";
+import { getCoupleById } from "@/app/utils/getCoupleById";
+import { Metadata } from "next";
+
+export async function generateMetadata({ params }): Promise<Metadata> {
+  const couple = await getCoupleById(params.id);
+  
+  return {
+    title: `${couple.people[0].name} and ${couple.people[1].name} (${couple.origin}) - Everything Sapphic`,
+    description: `Information about ${couple.people[0].name} and ${couple.people[1].name} in ${couple.origin} for sapphics, by sapphic. Expect like screen time, tropes, levels of homophobia depicted, etc.`,
+  }
+}
 
 export default async function Home({ searchParams, params }) {
   const infoId = params.id
