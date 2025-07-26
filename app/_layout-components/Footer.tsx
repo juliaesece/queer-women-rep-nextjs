f5a0f6b876f16a59b853c60e32ad2a6f5f38bb2b
Julia Sepulveda <ecojuliase@gmail.com>
2025-07-26 20:25:07 +0100

    Added tally form link

--- app/_layout-components/Footer.tsx
+++ app/_layout-components/Footer.tsx
@@ -1,5 +1,6 @@
 import st from "./footer.module.css"
 import { getDiscordLink } from "../utils/getDiscordLink";
+import Link from "next/link";
 
 export default async function Footer() {
   const discordLink = await getDiscordLink()
@@ -8,7 +9,7 @@ export default async function Footer() {
     <footer className={st.footer}>
       <div>
         <p>Hi! This is a work in progress, and is currently not production ready, so you might see some bugs, and the site might break down at times.</p>
-        <p>If you&apos;d like to help by reporting some bugs or suggesting some features, or just vibe, you can join the discord <a href={discordLink.link}>here</a> (link valid till {discordLink.validUntil}).</p>
+        <p>If you&apos;d like to help by reporting some bugs or suggesting some features, or just vibe, you can fill <Link href="https://tally.so/r/wMpeB0">this form</Link> or join the discord <a href={discordLink.link}>here</a> (link valid till {discordLink.validUntil}).</p>
       </div>
     </footer>
   );
