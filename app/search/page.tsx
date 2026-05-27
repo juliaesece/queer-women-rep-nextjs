b2e5d274d89667be9d4a124f9ebc2014441db3f1
Julia Sepulveda <ecojuliase@gmail.com>
2026-05-27 12:47:40 +0100

    Fixed search error

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -99,7 +99,7 @@ export async function generateMetadata({ searchParams }: Props) {
     const resSearchParams = await searchParams
 
     const hasFilters = Object.keys(resSearchParams).length > 0
-    if (!hasFilters) return {
+    if (!hasFilters || resSearchParams.info) return {
         title: "Everything Sapphic - Search and filter for lesbian, bisexual and queer women media to watch",
         description: "Advanced search our collaborative catalog of sapphic (lesbian, bisexual and queer women and enbies) representation in media (TV Shows, movies, or books), where you can filter for the kind of representation you're searching for, be it POC or happy endings.",
     };
